850474fdb4e4ac1b3d6396602f35d182ec9534ce
Panny2507 <sajbananna2005@gmail.com>
2025-07-01 11:27:12 +0200

    appmodulelike

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,9 +3,12 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LikeModule } from './like/like.module';
+
+
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
+  imports: [PrismaModule.forRoot({ isGlobal: true }), LikeModule],
   controllers: [AppController],
   providers: [AppService],
 })
