f10f60d50a5e5a7d6a782b604de5f40bc35bcf0d
Soma Henter <somahenter@gmail.com>
2025-08-18 16:11:29 +0200

    Include Follow.module in app

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { PrismaModule } from 'nestjs-prisma';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FollowModule } from './follow/follow.module';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true }), PostModule, UserModule],
+  imports: [PrismaModule.forRoot({ isGlobal: true }), PostModule, UserModule, FollowModule],
   controllers: [AppController],
   providers: [AppService],
 })
