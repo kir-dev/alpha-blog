22328a94f21ad7bd31119ffdfc4be21cebaff1bd
Soma Henter <somahenter@gmail.com>
2025-08-25 11:06:41 +0200

    Follow auth implementation

--- apps/backend/src/follow/follow.controller.ts
+++ apps/backend/src/follow/follow.controller.ts
@@ -1,17 +1,21 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UseGuards } from '@nestjs/common';
 import { FollowDto } from './dto/follow.dto';
 import { Follow } from './entities/follow.entity';
 import { FollowService } from './follow.service';
+
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users/:id')
 export class FollowController {
   constructor(private readonly followService: FollowService) {}
 
   @Post('follow')
+  @UseGuards(AuthGuard('jwt'))
   async followUser(@Param('id', ParseIntPipe) followingId: number, @Body() followDto: FollowDto): Promise<Follow> {
     return this.followService.followUser(followDto.followerId, followingId);
   }
   @Delete('unfollow')
+  @UseGuards(AuthGuard('jwt'))
   async unfollowUser(@Param('id', ParseIntPipe) followingId: number, @Body() followDto: FollowDto): Promise<void> {
     return this.followService.unfollowUser(followDto.followerId, followingId);
   }
