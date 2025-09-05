0e9188563286c228fa8110d32a71a281916db773
Soma Henter <somahenter@gmail.com>
2025-09-05 10:15:52 +0200

    Follow entity fix

--- apps/backend/src/follow/entities/follow.entity.ts
+++ apps/backend/src/follow/entities/follow.entity.ts
@@ -1,11 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 
 export class Follow {
   @IsNotEmpty()
-  @IsString()
+  @IsNumber()
   followerId: number;
 
   @IsNotEmpty()
-  @IsString()
+  @IsNumber()
   followingId: number;
 }
